f7a13986b686c2ee708a063f63825716842785a1
NathFromLuna <natalysc366@gmail.com>
2021-08-27 15:50:07 -0500

    primeras pantallas

---
+++ ProyectofinalInicial/app/src/main/java/fcfm/poi/proyectoFinal/MainActivity.kt
@@ -0,0 +1,11 @@
+package fcfm.poi.proyectoFinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
